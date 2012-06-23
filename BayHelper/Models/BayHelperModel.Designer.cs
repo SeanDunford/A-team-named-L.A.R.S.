5e9649d1fc6fbe53f56f3ac46b365eee20164237
arokotyan <arokotyan@madmobile.com>
2012-06-23 17:23:10 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	BayHelper/Models/BayHelperDbContextModel.Context.cs
    	BayHelper/Models/BayHelperModel.edmx

--- BayHelper/Models/BayHelperModel.Designer.cs
+++ BayHelper/Models/BayHelperModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\dev\A-team-named-L.A.R.S\BayHelper\Models\BayHelperModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Development\BayHelper\BayHelper\Models\BayHelperModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
