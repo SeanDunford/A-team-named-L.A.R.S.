f0719f5d29d5d6c4ef1fca5e21590c3812e7f8af
Luis Cintron <lcintron@madmobile.com>
2012-06-23 23:46:40 -0400

    Display events on home

--- BayHelper/Controllers/HomeController.cs
+++ BayHelper/Controllers/HomeController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BayHelper.Com.Models;
 
 namespace BayHelper.Com.Controllers
 {
     public class HomeController : Controller
     {
+        BayHelperEntities db = new BayHelperEntities();
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
+            var tst = db.Events.Where(e => e.DueDate > DateTime.Now).OrderByDescending(e => e.EventID).Take(5).ToList();
+            ViewBag.Latest = db.Events.Where(e => e.DueDate > DateTime.Now).OrderByDescending(e => e.EventID).Take(5).ToList();
+            ViewBag.Popular = db.Events.Where(e => e.DueDate > DateTime.Now).OrderByDescending(e => e.TimeDonations.Count() + e.ResourceDonations.Count() + e.FinanceDonations.Count()).Take(5).ToList();
             return View();
         }
 
